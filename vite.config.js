beb4d31ae9bfa2e90aeb0f42371b90d3478611fa
klebebande <klebebande161@gmail.com>
2024-06-04 01:08:31 +0200

    redirect

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
-  server: {
-    historyApiFallback: true,
+  build: {
+    outDir: 'dist',
   },
 });
