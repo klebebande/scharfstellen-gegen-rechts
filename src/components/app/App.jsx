833e488c3c44c8b91b91cb52eed3fc48e096b44a
klebebande <klebebande161@gmail.com>
2024-06-04 23:13:00 +0200

    add home

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -13,18 +13,21 @@ import SubpageFilterblase from "../sticker/Filterblase";
 import SubpageTaeter from "../sticker/Taeter";
 import SubpageHerzen from "../sticker/Herzen";
 
+import NotFound from "../notFound/NotFound";
+
 const App = () => {
   return (
     <div className="App container-fluid p-0">
       <Router>
         <Routes>
-          <Route path="/" element={<h1>Scharfstellen gegen Rechts!</h1>} />
+          <Route path="/" element={<Home />}  />
           <Route path="/nazis" element={<SubpageNazis />} />
           <Route path="/hetze" element={<SubpageHetze />} />
           <Route path="/kartoffel" element={<SubpageKartoffel />} />
           <Route path="/filterblase" element={<SubpageFilterblase />} />
           <Route path="/taeter" element={<SubpageTaeter />} />
           <Route path="/herzen" element={<SubpageHerzen />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
     </div>
