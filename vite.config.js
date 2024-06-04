b5ccef0e9c162ec02ba0b8a99e95af18e28d369f
klebebande <klebebande161@gmail.com>
2024-06-04 23:03:56 +0200

    new redirects

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/',
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
+  base: '/', // Ensure this is set to the correct base if deploying to a subdirectory
 });
